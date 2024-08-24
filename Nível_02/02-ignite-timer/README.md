ed6322a59e4f72d930ea73074e2d6e57ba7ff657
Felipe Monteiro <felipedeamonteiro@gmail.com>
2024-08-24 03:50:57 -0300

    Add chapters 1, 2 and 3 from 02-class

---
+++ Nível_02/02-ignite-timer/README.md
@@ -0,0 +1,23 @@
+# 02 - Criando SPAs com ReactJS
+
+Aqui terão algumas anotações pessoais com relação ao projeto sobre coisas novas que aprendi
+
+## Como rodar
+O projeto foi feito com Node na versão v20.10.0.
+
+Primeiramente intale as dependências:
+```
+npm install
+```
+E depois rode:
+```
+npm run dev
+```
+
+## Anotações
+
+Na aplicação estamos utilizando a configuração da rocketseat para o eslint. Para rodar o linting e fazer ele aparecer no terminal o resultado da análise, rode:
+
+```
+npx eslint src --ext .ts,.tsx
+```
---
+++ Nível_02/02-ignite-timer/src/pages/Home/index.tsx
@@ -0,0 +1,89 @@
+import { Play } from 'phosphor-react';
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+  MinutesAmountInput,
+} from './styles';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as zod from 'zod';
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+});
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+
+export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  });
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(data);
+    reset();
+  }
+
+  const task = watch('task');
+  const isSubmitDisabled = !task;
+
+  return (
+    <HomeContainer>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            list="tasks-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
+          />
+
+          <datalist id="tasks-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
+          />
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  );
+}
