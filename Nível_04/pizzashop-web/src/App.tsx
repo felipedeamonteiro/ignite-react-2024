041a8d617dd158aac469ac1d083a1396e4ab4c0f
Felipe Monteiro <felipedeamonteiro@gmail.com>
2024-09-24 01:50:06 -0300

    Add header buttons and orders table

--- Nível_04/pizzashop-web/src/App.tsx
+++ Nível_04/pizzashop-web/src/App.tsx
@@ -3,13 +3,16 @@ import './global.css';
 import { router } from './routes';
 import { HelmetProvider, Helmet } from 'react-helmet-async';
 import { Toaster } from 'sonner';
+import { ThemeProvider } from './components/theme/theme-provider';
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | Pizza Shop" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | Pizza Shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
