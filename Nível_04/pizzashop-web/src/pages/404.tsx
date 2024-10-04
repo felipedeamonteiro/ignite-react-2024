6ce4612764f4b5de481443d908a3e69451b8679f
Felipe Monteiro <felipedeamonteiro@gmail.com>
2024-10-04 01:57:04 -0300

    Add all layout for pizzashop

---
+++ Nível_04/pizzashop-web/src/pages/404.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+export function NotFound() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
+      <h1 className="text-4xl font-bold">404</h1>
+      <h2 className="text-3xl font-bold">Página não encontrada</h2>
+      <p className="text-accent-foreground">
+        Voltar para o{' '}
+        <Link to="/" className="text-sky-500 dark:text-sky-400">
+          Dashboard
+        </Link>
+      </p>
+    </div>
+  );
+}
