abc5cb67e9ed7632819cb892da47b143f1e601c7
Felipe Monteiro <felipedeamonteiro@gmail.com>
2025-01-20 23:22:31 -0300

    Add orders list and pagination

--- Nível_04/pizzashop-web/src/pages/app/orders/order-table-row.tsx
+++ Nível_04/pizzashop-web/src/pages/app/orders/order-table-row.tsx
@@ -3,10 +3,21 @@ import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
 import { TableCell, TableRow } from '@/components/ui/table';
 import { Search, ArrowRight, X } from 'lucide-react';
 import { OrderDetails } from './order-details';
+import { OrderStatus } from '@/components/order-status';
+import { formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
-// export interface OrderTableRowProps {}
+export interface OrderTableRowProps {
+  order: {
+    orderId: string;
+    createdAt: string;
+    status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered';
+    customerName: string;
+    total: number;
+  };
+}
 
-export function OrderTableRow() {
+export function OrderTableRow({ order }: OrderTableRowProps) {
   return (
     <TableRow>
       <TableCell>
@@ -24,19 +35,24 @@ export function OrderTableRow() {
         </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
-        sdjfosdjfsdjfpsdjfpodsjf
+        {order.orderId}
       </TableCell>
-      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+      <TableCell className="text-muted-foreground">
+        {formatDistanceToNow(order.createdAt, {
+          locale: ptBR,
+          addSuffix: true,
+        })}
+      </TableCell>
       <TableCell>
-        <div className="flex items-center gap-2">
-          <span className="h-2 w-2 rounded-full bg-slate-400" />
-          <span className="font-medium text-muted-foreground">Pendente</span>
-        </div>
+        <OrderStatus status={order.status} />
       </TableCell>
+      <TableCell className="font-medium">{order.customerName}</TableCell>
       <TableCell className="font-medium">
-        Felipe de Alcântara Monteiro
+        {order.total.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        })}
       </TableCell>
-      <TableCell className="font-medium">R$ 149,90</TableCell>
       <TableCell>
         <Button variant="outline" size="xs">
           <ArrowRight className="mr-2 h-3 w-3" />
