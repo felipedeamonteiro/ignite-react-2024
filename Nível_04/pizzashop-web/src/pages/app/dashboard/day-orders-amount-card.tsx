9c2c7c3c3b241e1f3171d51d0dfb653f28967ddc
Felipe Monteiro <felipedeamonteiro@gmail.com>
2025-02-26 23:29:37 -0300

    Add dinamic metrics to dashboard cards

--- Nível_04/pizzashop-web/src/pages/app/dashboard/day-orders-amount-card.tsx
+++ Nível_04/pizzashop-web/src/pages/app/dashboard/day-orders-amount-card.tsx
@@ -1,20 +1,46 @@
+import { getDayOrdersAmount } from '@/api/get-day-orders-amount';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { useQuery } from '@tanstack/react-query';
 import { Utensils } from 'lucide-react';
 
 export function DayOrdersAmountCard() {
+  const { data: dayOrdersAmount } = useQuery({
+    queryFn: getDayOrdersAmount,
+    queryKey: ['metrics', 'day-orders-amount'],
+  });
+
   return (
     <Card>
-      <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
+      <CardHeader className="flex-row items-center justify-between pb-2 space-y-0">
         <CardTitle className="text-base font-semibold">Pedidos (dia)</CardTitle>
-        <Utensils className="h-4 w-4 text-muted-foreground" />
+        <Utensils className="w-4 h-4 text-muted-foreground" />
       </CardHeader>
 
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">12</span>
-        <p className="text-xs text-muted-foreground">
-          <span className="text-rose-500 dark:text-rose-400">-4%</span> em
-          relação a ontem
-        </p>
+        {dayOrdersAmount && (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {dayOrdersAmount.amount.toLocaleString('pt-BR')}
+            </span>
+            <p className="text-xs text-muted-foreground">
+              {dayOrdersAmount.diffFromYesterday >= 0 ? (
+                <>
+                  <span className="text-emerald-500 dark:text-emerald-400">
+                    +{dayOrdersAmount.diffFromYesterday}%
+                  </span>{' '}
+                  em relação a ontem
+                </>
+              ) : (
+                <>
+                  <span className="text-rose-500 dark:text-rose-400">
+                    {dayOrdersAmount.diffFromYesterday}%
+                  </span>{' '}
+                  em relação a ontem
+                </>
+              )}
+            </p>
+          </>
+        )}
       </CardContent>
     </Card>
   );
